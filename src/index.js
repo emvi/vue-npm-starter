54622558717857abc75f9b54776513aef6105c0d
Marvin Blum <6115423+Kugelschieber@users.noreply.github.com>
2018-03-11 12:45:34 +0100

    Added vuex to index.js.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Vuex from "vuex";
 import * as pages from "./pages";
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const routes = [
 	{path: '/', component: pages.Home},
