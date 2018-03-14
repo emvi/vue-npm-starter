f679b460139b248ee6eeb7fe1e6dd7649d2f3352
Marvin Blum <6115423+Kugelschieber@users.noreply.github.com>
2018-03-14 11:50:44 +0100

    Changed vue-router mode to history and added axios interceptor for oauth token.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Vuex from "vuex";
+import axios from "axios";
 import * as pages from "./pages";
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
+// token interceptor for every request
+axios.interceptors.request.use((config) => {
+	const token = window.localStorage.getItem("token");
+
+	if(token){
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+
+	return config;
+}, (err) => {
+	return Promise.reject(err);
+});
+
 const routes = [
-	{path: '/', component: pages.Home},
-	{path: '*', component: pages.Home} // TODO 404
+	{path: "/", component: pages.Home},
+	{path: "*", component: pages.Home} // TODO 404
 ];
 
 new Vue({
 	el: "#app",
-	router: new VueRouter({routes})
+	router: new VueRouter({routes, mode: "history"})
 });
